0487d02723ddac1a69cd99045bd6151ae81ba790
Rmke223 <70180189+Rmke223@users.noreply.github.com>
2020-09-17 11:16:47 -0400

    master

---
+++ main.js
@@ -0,0 +1,33 @@
+
+setInterval(displayClock, 500);
+function displayClock () {
+   var time = new Date()
+   var hrs = time.getHours();
+   var min = time.getMinutes();
+   var sec = time.getSeconds();
+    var meridien = "A.M."
+
+    // sets am/pm and calulates and removes military time
+if (hrs > 12) {
+    hrs = hrs - 12;
+    meridien = "P.M."
+}
+//makes 0 hrs appears as 12
+if (hrs == 0) {
+    hrs = 12;
+}
+//adds zero in front of seconds if < 10
+if (sec < 10){
+    sec = "0" + sec;
+}
+
+//adds zero in front of minutes if < 10
+if (min < 10){
+    min = "0" + min;
+}
+
+document.getElementById('clock').innerHTML =  hrs  + ':' + min + ":" + sec + ' ' + meridien;
+
+}
+
+
